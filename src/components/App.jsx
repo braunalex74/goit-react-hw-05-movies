a675bff612875e36e7ae1664d35733a2e667271b
braunalex74 <braunalex74@googlemail.com>
2023-06-14 17:54:29 +0300

    add_Movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Router, Route, Routes } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
 import Home from './Home/Home';
 import Movies from './Movies/Movies';
 import MovieDetails from './MovieDetails/MovieDetails';
 import Cast from './Cast/Cast';
 import Reviews from './Reviews/Reviews';
 
+const history = createBrowserHistory();
+
 export const App = () => {
   return (
-    <Router>
+    <Router history={history}>
+      {' '}
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getTrendingMovies } from '../../api/api';
 
-export const Movies = () => (
-  <div>
-    <h2>Movies</h2>
-  </div>
-);
+export const Movies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchTrendingMovies();
+  }, []);
+
+  const fetchTrendingMovies = async () => {
+    try {
+      const response = await getTrendingMovies();
+      setMovies(response);
+    } catch (error) {
+      console.log('Error:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Movies</h2>
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 export default Movies;
