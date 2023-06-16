cf7acf239d41abdabf83b24f6bda53141a7bf577
braunalex74 <braunalex74@googlemail.com>
2023-06-16 19:59:16 +0300

    import_change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
+import { lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home/Home';
-import Movies from './Movies/Movies';
-import MovieDetails from './MovieDetails/MovieDetails';
-import Cast from './Cast/Cast';
-import Reviews from './Reviews/Reviews';
+const Home = lazy(() => import('./Home/Home'));
+const Movies = lazy(() => import('./Movies/Movies'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
-const App = () => {
+export const App = () => {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} />
-          <Route path="/movies/:movieId/cast" element={<Cast />} />
-          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />} />
+        <Route path="movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        <Route path="*" element={<div> Nothing found </div>} />
+      </Routes>
     </Router>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './components/App';
+import ReactDOM from 'react-dom/client';
+import App from 'components/App';
 import './index.css';
 
-render(
-  <Router>
-    <React.StrictMode>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </React.StrictMode>
-  </Router>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
