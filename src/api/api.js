3e7b237eff6593b23a0ce58f469ac65c384e9f58
braunalex74 <braunalex74@googlemail.com>
2023-06-17 22:59:02 +0300

    add_search

--- src/api/api.js
+++ src/api/api.js
@@ -57,3 +57,20 @@ export const getMovieReviews = async movieId => {
     throw new Error('Failed to fetch movie reviews');
   }
 };
+
+export const searchMovies = async query => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=104cb1f11a5d6d55ba8d6504d9bd060a&query=${encodeURIComponent(
+        query
+      )}`
+    );
+    if (!response.ok) {
+      throw new Error('Failed to search movies');
+    }
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    throw new Error('Failed to search movies');
+  }
+};
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,32 +1,78 @@
 import React, { useEffect, useState } from 'react';
-import { getTrendingMovies } from '../../api/api';
+import { searchMovies } from '../../api/api';
 import MovieList from '../MovieList/MovieList';
+import MovieDetails from '../MovieDetails/MovieDetails';
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedMovie, setSelectedMovie] = useState(null);
   const [error, setError] = useState(null);
+  const [searched, setSearched] = useState(false);
 
-  useEffect(() => {
-    fetchTrendingMovies();
-  }, []);
-
-  const fetchTrendingMovies = async () => {
+  const fetchSearchedMovies = async () => {
     try {
-      const trendingMovies = await getTrendingMovies();
-      setMovies(trendingMovies);
+      const searchedMovies = await searchMovies(searchQuery);
+      setMovies(searchedMovies);
     } catch (error) {
       setError(error.message);
     }
   };
 
+  const handleSearch = async () => {
+    setSearched(true);
+    fetchSearchedMovies();
+  };
+
+  const handleChange = e => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSelectMovie = movie => {
+    setSelectedMovie(movie);
+  };
+
+  const handleBack = () => {
+    setSelectedMovie(null);
+  };
+
+  useEffect(() => {
+    if (searched) {
+      fetchSearchedMovies();
+    }
+  }, [searched]);
+
   if (error) {
     return <div>Error: {error}</div>;
+  }
+
+  if (selectedMovie) {
+    return (
+      <div>
+        <button onClick={handleBack}>Back</button>
+        <MovieDetails movie={selectedMovie} />
+      </div>
+    );
   }
 
   return (
     <div>
       <h2>Movies</h2>
-      <MovieList movies={movies} />
+      {!selectedMovie && (
+        <div>
+          {!searched && (
+            <div>
+              <input type="text" value={searchQuery} onChange={handleChange} />
+              <button onClick={handleSearch}>Search</button>
+            </div>
+          )}
+          {searched && (
+            <div>
+              <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
